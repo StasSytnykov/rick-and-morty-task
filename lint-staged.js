d98ca333dc3f0e1802e113e413b1d8e07e3e1e45
StasSytnykov <stas82kak05@gmail.com>
2022-10-20 12:23:21 +0300

    add some dependencies and linter

--- lint-staged.js
+++ lint-staged.js
@@ -3,7 +3,6 @@ module.exports = {
     "eslint --max-warnings=0",
     "react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests",
     () => "tsc --skipLibCheck --noEmit",
-    "eslint --cache --fix",
   ],
   "*.{js,jsx,ts,tsx,json,css,js}": ["prettier --write"],
 };
