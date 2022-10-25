83397b6667748c4f5b82e34169dbe4d30aafb154
StasSytnykov <stas82kak05@gmail.com>
2022-10-25 17:21:57 +0300

    add property to initialstate

--- src/redux/characters/charactersSlice.ts
+++ src/redux/characters/charactersSlice.ts
@@ -3,31 +3,33 @@ import { createSlice } from "@reduxjs/toolkit";
 // // import type { PayloadAction } from "@reduxjs/toolkit";
 // // import { RootState } from "../store";
 //
-// type TCharacter = {
-//   id?: number;
-//   name?: string;
-//   image?: string;
-// };
-//
-// interface IinitialState {
-//   characters: TCharacter[];
-//   status: string;
-//   error: null | string;
-// }
-//
-// const initialState: IinitialState = {
-//   characters: [],
-//   status: "idle",
-//   error: null,
-// };
+type TCharacter = {
+  id?: number;
+  name?: string;
+  image?: string;
+};
+
+interface IinitialState {
+  characters: TCharacter[];
+  status: string;
+  error: null | string;
+}
+
+const initialState: IinitialState = {
+  characters: [],
+  status: "idle",
+  error: null,
+};
 
 export const charactersSlice = createSlice({
   name: "characters",
-  initialState: { characters: [] },
+  initialState,
   reducers: {
     fetchData: (state, action) => {
       return {
         characters: action.payload,
+        status: "success",
+        error: null,
       };
     },
   },
