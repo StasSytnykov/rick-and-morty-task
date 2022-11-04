05d79e5b6f948cbf2f991ec9987cdcee246d63c4
StasSytnykov <stas82kak05@gmail.com>
2022-11-04 13:34:43 +0200

    added fetch location test

--- src/api/__test__/fetchData.test.ts
+++ src/api/__test__/fetchData.test.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { fetchCharacters } from "../fetchData";
+import axios, { AxiosResponse } from "axios";
+import { fetchCharacters, fetchLocation } from "../fetchData";
 
 jest.mock("axios");
 
@@ -11,51 +11,75 @@ describe("fetchAllCharacters", () => {
   });
 
   test("should return characters", async () => {
-    mockedAxios.get.mockResolvedValue({
-      data: {
-        results: [
-          {
-            id: 1,
-            name: "Rick Sanchez",
-            status: "alive",
-            species: "human",
-            gender: "male",
-            image: "s",
-            episode: ["s", "2"],
-          },
-          {
-            id: 2,
-            name: "Morty",
-            status: "alive",
-            species: "human",
-            gender: "male",
-            image: "s",
-            episode: ["s", "2"],
-          },
-        ],
-      },
-    });
+    const charactersData = {
+      results: [
+        {
+          id: 1,
+          name: "Rick Sanchez",
+          status: "alive",
+          species: "human",
+          gender: "male",
+          image: "s",
+        },
+        {
+          id: 2,
+          name: "Morty",
+          status: "alive",
+          species: "human",
+          gender: "male",
+          image: "s",
+        },
+      ],
+    };
 
+    const mockedResponse: AxiosResponse = {
+      data: charactersData,
+      status: 200,
+      statusText: "OK",
+      headers: {},
+      config: {},
+    };
+
+    mockedAxios.get.mockResolvedValue(mockedResponse);
+
+    expect(axios.get).not.toHaveBeenCalled();
     const characters = await fetchCharacters(1);
-    expect(characters).toEqual([
-      {
-        id: 1,
-        name: "Rick Sanchez",
-        status: "alive",
-        species: "human",
-        gender: "male",
-        image: "s",
-        episode: ["s", "2"],
+    expect(axios.get).toHaveBeenCalled();
+    expect(characters).toEqual(charactersData.results);
+  });
+
+  test("should return location data", async () => {
+    const locationsData = {
+      info: {
+        count: 42,
       },
-      {
-        id: 2,
-        name: "Morty",
-        status: "alive",
-        species: "human",
-        gender: "male",
-        image: "s",
-        episode: ["s", "2"],
-      },
-    ]);
+      results: [
+        {
+          id: 1,
+          name: "Earth",
+          residents: ["1", "2", "230"],
+        },
+        {
+          id: 2,
+          name: "Mars",
+          residents: ["50", "60"],
+        },
+      ],
+    };
+
+    const mockedResponse: AxiosResponse = {
+      data: locationsData,
+      status: 200,
+      statusText: "OK",
+      headers: {},
+      config: {},
+    };
+
+    mockedAxios.get.mockResolvedValue(mockedResponse);
+
+    expect(axios.get).not.toHaveBeenCalled();
+    const locations = await fetchLocation();
+    expect(axios.get).toHaveBeenCalled();
+    expect(locations).toEqual(locationsData);
   });
 });
