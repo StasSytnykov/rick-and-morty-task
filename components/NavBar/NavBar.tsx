51c768a968effead0e86c19948cd25638b2ea7dd
StasSytnykov <stas82kak05@gmail.com>
2022-11-16 13:04:30 +0200

    fixed style on navLinks

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -5,31 +5,40 @@ import {
   NavLinkThumb,
   NavLinkTextThumb,
 } from "./NavBar.styled";
+import { useRouter } from "next/router";
 
-export const NavBar = () => (
-  <NavBarStyled>
-    <WebSiteTitle>Welcome to Rick and Morty universe!</WebSiteTitle>
-    <NavLinkThumb>
-      <NavLinkTextThumb>
-        <Link
-          href={"/"}
-          // style={({ isActive }) => ({
-          //   color: isActive ? "rgb(218, 75, 12)" : "rgb(255, 255, 255)",
-          // })}
-        >
-          Home page
-        </Link>
-      </NavLinkTextThumb>
-      <NavLinkTextThumb>
-        <Link
-          href={"/statistics"}
-          // style={({ isActive }) => ({
-          //   color: isActive ? "rgb(218, 75, 12)" : "rgb(255, 255, 255)",
-          // })}
-        >
-          Statistics page
-        </Link>
-      </NavLinkTextThumb>
-    </NavLinkThumb>
-  </NavBarStyled>
-);
+export const NavBar = () => {
+  const router = useRouter();
+  const path = router.pathname;
+  return (
+    <NavBarStyled>
+      <WebSiteTitle>Welcome to Rick and Morty universe!</WebSiteTitle>
+      <NavLinkThumb>
+        <NavLinkTextThumb>
+          <Link
+            href={"/"}
+            style={
+              path === "/"
+                ? { color: "rgb(218, 75, 12)" }
+                : { color: "rgb(255, 255, 255)" }
+            }
+          >
+            Home page
+          </Link>
+        </NavLinkTextThumb>
+        <NavLinkTextThumb>
+          <Link
+            href={"/statistics"}
+            style={
+              path === "/statistics"
+                ? { color: "rgb(218, 75, 12)" }
+                : { color: "rgb(255, 255, 255)" }
+            }
+          >
+            Statistics page
+          </Link>
+        </NavLinkTextThumb>
+      </NavLinkThumb>
+    </NavBarStyled>
+  );
+};
--- components/Statistics/Statistics.tsx
+++ components/Statistics/Statistics.tsx
@@ -1,29 +1,38 @@
 import Link from "next/link";
 import { NavLinkThumb, NavLinkTextThumb } from "./Statistics.styled";
+import { useRouter } from "next/router";
 
-export const Statistics = () => (
-  <>
-    <NavLinkThumb>
-      <NavLinkTextThumb>
-        <Link
-          href={"/statistics/episodes"}
-          // style={({ isActive }) => ({
-          //   color: isActive ? "rgb(218, 75, 12)" : "black",
-          // })}
-        >
-          Episodes
-        </Link>
-      </NavLinkTextThumb>
-      <NavLinkTextThumb>
-        <Link
-          href={"/statistics/location"}
-          // style={({ isActive }) => ({
-          //   color: isActive ? "rgb(218, 75, 12)" : "black",
-          // })}
-        >
-          Location
-        </Link>
-      </NavLinkTextThumb>
-    </NavLinkThumb>
-  </>
-);
+export const Statistics = () => {
+  const router = useRouter();
+  const path = router.pathname;
+  return (
+    <>
+      <NavLinkThumb>
+        <NavLinkTextThumb>
+          <Link
+            href={"/statistics/episodes"}
+            style={
+              path === "/statistics/episodes"
+                ? { color: "rgb(218, 75, 12)" }
+                : { color: "black" }
+            }
+          >
+            Episodes
+          </Link>
+        </NavLinkTextThumb>
+        <NavLinkTextThumb>
+          <Link
+            href={"/statistics/location"}
+            style={
+              path === "/statistics/location"
+                ? { color: "rgb(218, 75, 12)" }
+                : { color: "black" }
+            }
+          >
+            Location
+          </Link>
+        </NavLinkTextThumb>
+      </NavLinkThumb>
+    </>
+  );
+};
